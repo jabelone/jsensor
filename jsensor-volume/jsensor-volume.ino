630920865b2144719b3ef598dd210cbc42556cab
Jaimyn Mayer <admin@jabelone.com>
2017-01-26 05:48:33 +1000

    Cleaned up jsensor-volume.

--- jsensor-volume/jsensor-volume.ino
+++ jsensor-volume/jsensor-volume.ino
@@ -1,102 +1,91 @@
 #include <PubSubClient.h>
 #include <ESP8266WiFi.h>
+#include "RunningAverage.h"
 
-const char* ssid = "HSBNEWiFi";
-const char* password = "HSBNEPortHack";
+//WiFi Settings
+const char* WiFiSSID = "HSBNEWiFi";
+const char* WiFiPassword = "HSBNEPortHack";
 
-String topic = "jsensor/bathroom/volume";
-char* server = "10.0.0.199";
+//MQTT Settings
+String MQTTclientName = "jsensor-bathroom";
+String MQTTtopic = "jsensor/bathroom/volume";
+char* MQTTserver = "10.0.0.199";
 
+//Other Settings
+const int minTrigger = 15;
+const int signalPin = D5;
+const int threstholdSeconds = 10;
+RunningAverage myRA(10); //Samples to store for average
+
+//Program variables
+bool triggered = 0;
+int samples = 0;
+
+//Called when MQTT topic is received
 void callback(char* topic, byte* payload, unsigned int length) {
-  // handle message arrived
 }
 
 WiFiClient wifiClient;
-PubSubClient client(server, 1883, callback, wifiClient);
-
-String macToStr(const uint8_t* mac)
-{
-  String result;
-  for (int i = 0; i < 6; ++i) {
-    result += String(mac[i], 16);
-    if (i < 5)
-      result += ':';
-  }
-  return result;
-}
-
-#define signalPin D5
-int threstholdTime = 5000;
-bool triggered = 0;
+PubSubClient client(MQTTserver, 1883, callback, wifiClient);
 
 void setup() {
+  pinMode(signalPin, INPUT);
   Serial.begin(115200);
-  delay(10);
-  
   Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  
-  WiFi.begin(ssid, password);
-  
+  Serial.print("Attemtping WiFi connection to ");
+  Serial.println(WiFiSSID);
+
+  WiFi.begin(WiFiSSID, WiFiPassword);
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
-  Serial.println("WiFi connected");  
-  Serial.println("IP address: ");
+  Serial.println("Connected to " + String(WiFiSSID));
+  Serial.print("Got IP address: ");
   Serial.println(WiFi.localIP());
 
-  // Generate client name based on MAC address and last 8 bits of microsecond counter
-  String clientName;
-  clientName += "esp8266-";
-  uint8_t mac[6];
-  WiFi.macAddress(mac);
-  clientName += macToStr(mac);
-  clientName += "-";
-  clientName += String(micros() & 0xff, 16);
-
-  Serial.print("Connecting to ");
-  Serial.print(server);
+  Serial.print("Attemtping MQTT connection to ");
+  Serial.print(MQTTserver);
   Serial.print(" as ");
-  Serial.println(clientName);
-  
-  if (client.connect((char*) clientName.c_str())) {
+  Serial.println(MQTTclientName);
+
+  if (client.connect((char*) MQTTclientName.c_str())) {
     Serial.println("Connected to MQTT broker");
-    Serial.print("Topic is: ");
-    Serial.println(topic);
-    
-      String temptopic = topic + "/status";
-      client.publish(temptopic.c_str(), "boot complete");
-      client.publish(topic.c_str(), "0");
+    Serial.print("Will publish to topic: ");
+    Serial.println(MQTTtopic);
+
+    String temptopic = MQTTtopic + "/status";
+    client.publish(temptopic.c_str(), "boot complete");
+    client.publish(MQTTtopic.c_str(), "0");
   }
   else {
-    Serial.println("MQTT connect failed");
+    Serial.println("Couldn't connect to MQTT Broker");
     Serial.println("Will reset and try again...");
     abort();
   }
-
-  pinMode(signalPin, INPUT);
+  myRA.clear(); // explicitly start clean
 }
 
 void loop() {
 
-  if (!triggered && digitalRead(signalPin)) {
-    delay(threstholdTime);
-    if(digitalRead(signalPin)) {
-      triggered = 1;
-      Serial.println("triggered");
-      client.publish(topic.c_str(), "1");
+  if (millis() % 10 == 0) {
+    myRA.addValue(digitalRead(signalPin));
+    Serial.println(MQTTtopic + myRA.getAverage());
+    samples++;
+    if (samples >= threstholdSeconds * 100) {
+      samples = 0;
+      myRA.clear();
     }
   }
-  else if (triggered && !digitalRead(signalPin)) {
-    delay(threstholdTime);
-    if(!digitalRead(signalPin)) {
-      triggered = 0;
-      Serial.println("not triggered");
-      client.publish(topic.c_str(), "0");
-    }
+  if (!triggered && myRA.getAverage() > 0.5) {
+    triggered = 1;
+    client.publish(MQTTtopic.c_str(), "1");
+    delay(minTrigger * 1000);
+  }
+  else if (triggered && myRA.getAverage() < 0.5) {
+    triggered = 0;
+    client.publish(MQTTtopic.c_str(), "0");
   }
 }
